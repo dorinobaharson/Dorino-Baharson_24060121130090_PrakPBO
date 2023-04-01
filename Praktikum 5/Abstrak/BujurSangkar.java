eed92e74f8582c57e6d7745725cc69b783b61021
dorinobaharson <89585868+dorinobaharson@users.noreply.github.com>
2023-04-01 07:26:19 +0700

    Update BujurSangkar.java

--- Praktikum 5/Abstrak/BujurSangkar.java
+++ Praktikum 5/Abstrak/BujurSangkar.java
@@ -1,3 +1,10 @@
+/*
+ * File : BangunDatar.java
+ * Penulis : Dorino Baharson
+ * tanggal : 3/30/2023
+ * Deskripsi : kelas yang membuat implementasi metode abstrak pada bangun datar
+ */
+
 package Abstrak;
 
 

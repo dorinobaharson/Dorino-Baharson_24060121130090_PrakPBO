ab9bdf00b66e8c0da693792d35069ef59c1ca357
dorinobaharson <89585868+dorinobaharson@users.noreply.github.com>
2023-04-01 07:28:52 +0700

    Update IArea.java

--- Praktikum 5/Interface/IArea.java
+++ Praktikum 5/Interface/IArea.java
@@ -1,6 +1,6 @@
 
 /*
- * File : BangunDatar.java
+ * File : IArea.java
  * Penulis : Dorino Baharson
  * tanggal : 3/30/2023
  * Deskripsi : interface yang mendefinisikan kontrak bahwa setiap kelas yang membuat implmentasi dari interface ini harus memiliki cara untuk menghitung luas

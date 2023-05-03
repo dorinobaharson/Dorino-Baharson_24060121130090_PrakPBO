050dd4000da0506ff6e26caedfbbbe8e3963c32b
dorinobaharson <89585868+dorinobaharson@users.noreply.github.com>
2023-05-03 19:33:46 +0700

    Delete Praktikum 7/Praktikum 7 (Vehicle) directory

--- Praktikum 7/Praktikum 7 (Vehicle)/Bus.java
+++
@@ -1,7 +0,0 @@
-public class Bus extends Vehicle{
-    void calRent(int distance, float price){
-        float fare = distance * price;
-        fare = fare - 50.00f;
-        System.out.println("Harga Sewa Bus: " + fare);
-    }
-}
